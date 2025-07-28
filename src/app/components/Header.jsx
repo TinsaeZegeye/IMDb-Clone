3bb5d7421bfd1afebe007b58fa9fa1bf6a66c582
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-07-28 03:48:11 +0300

    Istall next js and tailwind css and create header section

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+import MenuItem from './MenuItem'
+import { IoHome } from 'react-icons/io5' 
+import { TiInfoLarge } from "react-icons/ti";
+
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between sm:mx-auto max-w-6xl mx-2 py-6">
+      <div className='flex'>
+              <MenuItem title='HOME' address='/' Icon={IoHome} />
+              <MenuItem title='ABOUT' address='/about' Icon={TiInfoLarge} />
+          </div>
+          <div >
+              <Link href='/'>
+                  <h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>IMDb</span><span className='text-xl hidden sm:inline'>Clone</span></h2>
+              </Link>
+          </div>
+    </div>
+  );
+}
