5e1d18d22caddda4abe34feddc98570923bb535a
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-07-30 12:31:19 +0300

    fetch data from TBMDB to our home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import Image from "next/image"
+import Results from './components/Results'
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+
+  const rep = await fetch(
+    `https://api.themoviedb.org/3/${genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'}?api_key=7c7f7e0cc215dc207d1326ebf7c822bb&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!rep.ok) {
+    throw new Error("Failed to fetch Data!");
+  }
+
+  const data = await rep.json();
+
+  const results = data.results;
   return (
     <div>
-      Hello World!
+      <Results results={results} />
     </div>
   );
 }
